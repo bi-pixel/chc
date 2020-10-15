cb823f0ccb778747d86de4942e907317594b63c1
Oleh Zubra <zminig.1@gmail.com>
2020-10-15 18:19:17 +0300

    Test commit

--- install.sh
+++ install.sh
@@ -1,12 +1,67 @@
 #!/bin/bash
 
+# wget -O install.sh 'https://github.com/bi-pixel/chc/raw/master/install.sh'; bash install.sh
+
 clear
-echo "============================================================================================"
+echo "============================================================================================="
 echo "                              WELCOME TO CHC FAST DEPLOY!"
-echo "============================================================================================"
+echo "============================================================================================="
 echo
 echo "Hardening your OS..."
 echo "---------------------------"
 export DEBIAN_FRONTEND=noninteractive
-apt-get -qq update 
+apt-get -qq update
 apt-get -qq upgrade -y
+apt-get -y install python-virtualenv virtualenv
+cd ~ || exit
+mkdir -p .chaincoincore
+mkdir -p chaincoin
+wget -O chaincoin-0.16.4.tar.gz 'https://github.com/bi-pixel/chc/raw/master/chaincoin-0.16.4-x86_64-linux-gnu.tar.gz'
+wget -O sen.tar.gz 'https://github.com/bi-pixel/chc/raw/master/sen.tar.gz'
+tar xvzf chaincoin-0.16.4.tar.gz
+rm chaincoin-0.16.4.tar.gz
+mv chaincoin-0.16.4/bin/* chaincoin
+rm -r chaincoin-0.16.4/
+tar xvzf sen.tar.gz
+rm sen.tar.gz
+mv sentinel/ .chaincoincore/
+
+echo \
+"daemon=1
+listen=1
+rpcport=11995
+rpcallowip=127.0.0.1
+server=1
+rpcuser=user
+rpcpassword=password
+#masternode=1
+#masternodeprivkey=GENKEY
+#externalip=34.67.150.167:11994
+" | sudo tee .chaincoincore/chaincoin.conf
+
+cd .chaincoincore/sentinel/ || exit
+rm -rf venv
+virtualenv ./venv
+./venv/bin/pip install -r requirements.txt
+
+( crontab -l | cat; echo "* * * * * cd /root/.chaincoincore/sentinel && ./venv/bin/python bin/sentinel.py >/dev/null 2>&1" ) | crontab -
+( crontab -l | cat; echo "@reboot sudo /sbin/iptables -t nat -A OUTPUT -s 10.128.0.5 -d 34.67.150.167 -j DNAT --to-destination 127.0.0.1" ) | crontab -
+( crontab -l | cat; echo "@reboot /root/chaincoin/chaincoind" ) | crontab -
+( crontab -l | cat; echo "*/20 * * * * /root/chaincoin/chaincoind" ) | crontab -
+
+# создаем файл подкачки
+# Получите список имен и путей файлов подкачки для вашей системы
+swapon -s
+# Выключите и удалите файл подкачки, если он уже существует
+swapoff /swapfile
+rm /swapfile
+# Выделите файл подкачки, при необходимости изменив команду
+fallocate -l 1G /swapfile
+# Установите разрешения для файла подкачки
+chmod 600 /swapfile
+# Отформатируйте файл как пространство подкачки
+mkswap /swapfile
+# Активировать своп
+swapon /swapfile
+# Отредактируйте файл / etc / fstab, указав, что своп должен монтироваться при загрузке
+echo '/swapfile none swap sw 0 0' | tee -a /etc/fstab
